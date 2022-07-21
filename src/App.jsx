90764bc690aaefd81e3798a2a87c5e776cb68824
Arturo de la Fuente <arturosdev94@gmail.com>
2022-07-20 22:27:48 -0400

    exercise done for portfolio, ready to adapt

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="card">
+      <div className="circle" >
+        <img src="https://pngimg.com/uploads/cocacola_logo/cocacola_logo_PNG13.png" alt="coca-logo" className='logo'/>
+      </div>
+      <div className="content">
+        <h2>Coke</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rerum laborum architecto voluptatum sint consequuntur natus, quam possimus molestiae ex qui maxime voluptate beatae corporis ad distinctio veniam voluptatibus ratione!</p>
+        <a href="#">Explore</a>
+      </div>
+      <img src="https://pngimg.com/uploads/cocacola/cocacola_PNG6.png" alt="coke" className='product' />
+    </div>
+    <div className="card">
+    <div className="circle" >
+      <img src="https://pngimg.com/uploads/cocacola_logo/cocacola_logo_PNG13.png" alt="coca-logo" className='logo'/>
+    </div>
+    <div className="content">
+      <h2>Coke</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rerum laborum architecto voluptatum sint consequuntur natus, quam possimus molestiae ex qui maxime voluptate beatae corporis ad distinctio veniam voluptatibus ratione!</p>
+      <a href="#">Explore</a>
+    </div>
+    <img src="https://pngimg.com/uploads/cocacola/cocacola_PNG6.png" alt="coke" className='product' />
+  </div>
+  </>
+  )
+}
+
+export default App
